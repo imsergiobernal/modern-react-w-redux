f5eeca2531bfb9a95ff49fad7d993eb0639e5830
Sergio Bernal Guillot <sergioguillot@gmail.com>
2018-05-14 00:43:12 +0200

    Play video after clicking on component

---
+++ src/components/VideoList/video_list.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import VideoListItem from './video_list_item'
+
+const VideoList = (props) => {
+    const VideoItems = props.videos.map((video) => {
+        return <VideoListItem
+            key={video.etag}
+            onVideoSelect={props.onVideoSelect}
+            video={video} />
+    })
+
+    return (
+        <ul className="list-group">
+            {VideoItems}
+        </ul>
+    )
+}
+
+export default VideoList
