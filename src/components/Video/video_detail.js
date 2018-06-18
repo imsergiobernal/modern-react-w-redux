4e4c4328969ded577f539ad29df178bc81828ed2
Sergio Bernal Guillot <sergioguillot@gmail.com>
2018-06-18 18:32:41 +0200

    debounce search and keypress video toggle

--- src/components/Video/video_detail.js
+++ src/components/Video/video_detail.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
-const VideoDetail = ({video}) => {
+const VideoDetail = ({video, isPlaying}) => {
     if (!video) {
         return <div>Loading...</div>
     }
 
     const videoId = video.id.videoId
-    const url = `https://www.youtube.com/embed/${video.id.videoId}`
+    const url = `https://www.youtube.com/watch?v=${video.id.videoId}`
 
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" src={url}></iframe>
+                <ReactPlayer
+                    url={url}
+                    height='100%'
+                    playing={isPlaying}
+                />
             </div>
             <div className="details">
                 <div>{video.snippet.title}</div>
-                <div>{video.snippet.description}</div>
+                <div style={{ 'fontSize':'12px' }}>{video.snippet.description}</div>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import VideoList from './components/VideoList/video_list'
 import VideoDetail from './components/Video/video_detail'
 import YTSearch from 'youtube-api-search'
 import registerServiceWorker from './registerServiceWorker';
+import _ from 'lodash';
 const YT_API_KEY = "AIzaSyB_3xM8R-1g6pBCUa0RnVEJpDRrQJtbs2U";
 
 class App extends React.Component
@@ -14,28 +15,45 @@ class App extends React.Component
     super(props)
     this.state = {
       videos: [],
-      selectedVideo: null
+      selectedVideo: null,
+      isPlaying: false
     } 
     this.videoSearch('cake')
   }
   
-  videoSearch(term) {
+  componentDidMount = () => {
+    document.addEventListener('keypress', this.handleKeyPress);
+  }
+
+  videoSearch = (term) => {
     YTSearch({ key: YT_API_KEY, term: term }, videos => {
       this.setState({
         videos,
-        selectedVideo: videos[0]
+        selectedVideo: videos[0],
+        isPlaying: false
       })
     })
   }
 
+  handleKeyPress = (event) => {
+    if (event.keyCode === 32){
+      this.setState({
+        isPlaying: !this.state.isPlaying
+      });
+      console.log(event.keyCode);
+    };
+  };
+
   render() {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 500);
+
     return(
       <div className="container">
         <div className="row">
-          <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+          <SearchBar onSearchTermChange={videoSearch} />
         </div>
         <div className="row">
-          <VideoDetail video={this.state.selectedVideo} />
+          <VideoDetail video={this.state.selectedVideo} isPlaying={this.state.isPlaying} />
           <VideoList
             onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
             videos={this.state.videos} />
